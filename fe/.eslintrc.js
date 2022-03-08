cf347249e18e30d769373bf36e5d2f9e26c3de26
Zelzahn <friedrich@friedrichvdb.be>
2022-03-08 10:41:27 +0100

    linting & formatting support

--- fe/.eslintrc.js
+++ fe/.eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
-    extends: [
-        'plugin:vue/vue3-essential',
-        'prettier',
-    ],
+    extends: ['plugin:vue/vue3-essential', 'prettier'],
+    parserOptions: {
+        ecmaVersio: 12,
+        sourceType: 'module',
+        parser: '@typescript-eslint/parser',
+    },
+    plugins: ['vue', '@typescript-eslint'],
 }
