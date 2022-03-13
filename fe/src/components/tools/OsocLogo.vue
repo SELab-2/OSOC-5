48f39a1e491ebe11708fc56330cd911f14daee36
Lisa De Jonghe <lisa.dejonghe@ugent.be>
2022-03-13 23:34:40 +0100

    added dark logo modification
    # Conflicts:
    #	fe/src/components/tools/OsocLogo.vue

--- fe/src/components/tools/OsocLogo.vue
+++ fe/src/components/tools/OsocLogo.vue
@@ -1,4 +1,14 @@
 <template>
-  <img v-if="$q.dark.isActive" style="max-width: min(80%, 550px); margin-left: max(10%, (100% - 550px)/2); filter: invert(0.25)"  src="https://osoc.be/img/logo/logo-osoc-color.svg" />
-  <img v-else style="display: block; margin-left: auto; margin-right: auto; width: 60%; max-height: 250px"  src="https://osoc.be/img/logo/logo-osoc-color.svg" />
+  <img v-if="$q.dark.isActive" src="../../assets/logo_dark.svg"/>
+  <img v-else src="../../assets/logo.svg"/>
 </template>
+
+<style scoped>
+img {
+  display: block; 
+  margin-left: auto; 
+  margin-right: auto; 
+  width: 60%; 
+  max-height: 250px;
+}
+</style>
