eff0280dfa598201bbd4143813c9c2035f23101c
Wouter01 <wouterhennen@gmail.com>
2022-03-13 19:05:44 +0100

    Changed shadow of buttons

--- fe/src/components/forms/LoginForm.vue
+++ fe/src/components/forms/LoginForm.vue
@@ -41,22 +41,23 @@
     <br/>
 
 <!--     <div> -->
-      <q-btn
+      <q-btn unelevated
           color="primary"
           label="Sign in"
           type="submit"
           size="md"
           class="q-mx-md cornered"
-          
+          style="box-shadow: 0px 5px 20px -4px #57bf98"
       />
 <!--     <br/> -->
       <label class="text-bold">or</label>
 <!--       <br/> -->
-      <q-btn
+      <q-btn unelevated
        color="black" 
        icon="mdi-github" 
        label="Sign in via GitHub" 
        class="q-mx-md cursor-pointer cornered"
+       style="box-shadow: 0px 8px 20px -4px grey"
        >
       </q-btn>
       
