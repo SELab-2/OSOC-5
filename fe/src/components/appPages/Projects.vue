a96047db92a2d83f7fed64ba0a926b43600119c2
lasaeren <lander.saerens@ugent.be>
2022-03-18 17:47:49 +0100

    project card: added info, actions and list

--- fe/src/components/appPages/Projects.vue
+++ fe/src/components/appPages/Projects.vue
@@ -1,40 +1,78 @@
 <template>
     <h2 style='font-weight: 800'>Projects</h2>
     <div class='q-pa-md row items-start q-gutter-md'>
-        <q-card class='my-card' flat bordered>
+
+
+        <q-card  v-for="(item, index) in [ {}, {}, {}, {}, {}, {}, {} ]" :key="index" class='my-card' flat bordered>
 
             <q-card-section>
-                <div class='text-overline text-orange-9'>Client 1</div>
                 <div class='text-h5 q-mt-sm q-mb-xs'>Project 1</div>
-                <div class='text-caption text-grey'>Coach #1</div>
-                <div class='text-caption text-grey'>Coach #2</div>
+                <div class='text-overline '>Client 1</div>
+                <div class='text-caption text-grey'>Coaches: Miet, Astrid</div>
+
             </q-card-section>
+            <q-list bordered class='rounded-borders' style='max-width: 600px'>
+
+<!--    LIJST VAN STUDENTEN            -->
+
+                <q-item-label header>Students:</q-item-label>
+
+
+                <q-item>
+                    <q-item-section top>
+                        <q-item-label lines='1'>
+                            <span class='text-weight-medium'>Dayana Stark</span>
+                        </q-item-label>
+                        <span class='text-grey-8'>Frond End Developer</span>
+                        <q-item-label caption lines='1'>
+                            Suggested by <span class='text-weight-medium'>Jeroen Dewelde</span>
+                        </q-item-label>
+                    </q-item-section>
+
+                    <q-item-section top side>
+                        <div class='text-grey-8 q-gutter-xs'>
+                            <q-btn class='gt-xs' size='12px' flat dense round icon='comment' />
+                            <q-btn class='gt-xs' size='12px' flat dense round icon='info' />
+                            <q-btn class='gt-xs' size='12px' flat dense round icon='delete' />
+                        </div>
+                    </q-item-section>
+                </q-item>
+                <q-separator spaced />
+
+                <q-item>
+                    <q-item-section top>
+                        <q-item-label lines='1'>
+                            <span class='text-weight-medium'>Dayana Stark</span>
+                        </q-item-label>
+                        <span class='text-grey-8'>Frond End Developer</span>
+                        <q-item-label caption lines='1'>
+                            Suggested by <span class='text-weight-medium'>Jeroen Dewelde</span>
+                        </q-item-label>
+                    </q-item-section>
+
+                    <q-item-section top side>
+                        <div class='text-grey-8 q-gutter-xs'>
+                            <q-btn class='gt-xs' size='12px' flat dense round icon='comment' />
+                            <q-btn class='gt-xs' size='12px' flat dense round icon='info' />
+                            <q-btn class='gt-xs' size='12px' flat dense round icon='delete' />
+                        </div>
+                    </q-item-section>
+                </q-item>
+                <!--                <q-separator spaced />-->
+            </q-list>
 
             <q-card-actions>
-                <q-btn flat color='dark' label='Edit' />
                 <q-btn flat color='primary' label='Mail' />
-
                 <q-space />
+                <q-btn flat color='dark' label='Edit' />
 
-                <q-btn
-                    color='grey'
-                    round
-                    flat
-                    dense
-                    :icon="expanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'"
-                    @click='expanded = !expanded'
-                />
             </q-card-actions>
 
-            <q-slide-transition>
-                <div v-show='expanded'>
-                    <q-separator />
-                    <q-card-section class='text-subitle2'>
-                        {{ lorem }}
-                    </q-card-section>
-                </div>
-            </q-slide-transition>
         </q-card>
+
+
+
+
         <q-page-sticky position='bottom-right' :offset='[18, 18]'>
             <q-btn fab icon='add' color='accent' to='/projects/create' />
         </q-page-sticky>
@@ -45,14 +83,7 @@
 import { ref } from 'vue'
 
 export default {
-
     name: 'Projects.vue',
-    setup() {
-        return {
-            expanded: ref(false),
-            lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        }
-    },
 }
 </script>
 
@@ -63,5 +94,5 @@ export default {
 <style lang='sass' scoped>
 .my-card
     width: 100%
-    max-width: 350px
+    max-width: 400px
 </style>
