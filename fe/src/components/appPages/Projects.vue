a529655a833f2c51b040aa894b452b4f68f70220
lasaeren <lander.saerens@ugent.be>
2022-03-18 17:55:11 +0100

    add tags for coaches and roles

--- fe/src/components/appPages/Projects.vue
+++ fe/src/components/appPages/Projects.vue
@@ -3,61 +3,54 @@
     <div class='q-pa-md row items-start q-gutter-md'>
 
 
-        <q-card  v-for="(item, index) in [ {}, {}, {}, {}, {}, {}, {} ]" :key="index" class='my-card' flat bordered>
+        <q-card v-for='(item, index) in [ {}, {}, {}, {}, {}, {}, {} ]' :key='index' class='my-card' flat bordered>
 
             <q-card-section>
+
                 <div class='text-h5 q-mt-sm q-mb-xs'>Project 1</div>
+
                 <div class='text-overline '>Client 1</div>
-                <div class='text-caption text-grey'>Coaches: Miet, Astrid</div>
+
+                <div class='text-caption text-grey'>Coaches:</div>
+                <q-chip outline icon='person'>Astrid</q-chip>
+                <q-chip outline icon='person'>Miet</q-chip>
+
+                <div class='text-caption text-grey'>Roles:</div>
+                <q-chip outline icon='info'>1x Full Stack Developer</q-chip>
+                <q-chip outline icon='info'>1x Comms</q-chip>
+                <q-chip outline icon='info'>1x Front-end Developer</q-chip>
+                <q-chip outline icon='info'>1x Media</q-chip>
+                <q-chip outline icon='info'>1x Back-end Developer</q-chip>
 
             </q-card-section>
             <q-list bordered class='rounded-borders' style='max-width: 600px'>
 
-<!--    LIJST VAN STUDENTEN            -->
+                <!--    LIJST VAN STUDENTEN            -->
 
                 <q-item-label header>Students:</q-item-label>
 
+                <div v-for='(item, index) in [ {}, {}, {}, {} ]' :key='index'>
+                    <q-item>
+                        <q-item-section top>
+                            <q-item-label lines='1'>
+                                <span class='text-weight-medium'>Dayana Stark</span>
+                            </q-item-label>
+                            <span class='text-grey-8'>Frond End Developer</span>
+                            <q-item-label caption lines='1'>
+                                Suggested by <span class='text-weight-medium'>Jeroen Dewelde</span>
+                            </q-item-label>
+                        </q-item-section>
 
-                <q-item>
-                    <q-item-section top>
-                        <q-item-label lines='1'>
-                            <span class='text-weight-medium'>Dayana Stark</span>
-                        </q-item-label>
-                        <span class='text-grey-8'>Frond End Developer</span>
-                        <q-item-label caption lines='1'>
-                            Suggested by <span class='text-weight-medium'>Jeroen Dewelde</span>
-                        </q-item-label>
-                    </q-item-section>
-
-                    <q-item-section top side>
-                        <div class='text-grey-8 q-gutter-xs'>
-                            <q-btn class='gt-xs' size='12px' flat dense round icon='comment' />
-                            <q-btn class='gt-xs' size='12px' flat dense round icon='info' />
-                            <q-btn class='gt-xs' size='12px' flat dense round icon='delete' />
-                        </div>
-                    </q-item-section>
-                </q-item>
-                <q-separator spaced />
-
-                <q-item>
-                    <q-item-section top>
-                        <q-item-label lines='1'>
-                            <span class='text-weight-medium'>Dayana Stark</span>
-                        </q-item-label>
-                        <span class='text-grey-8'>Frond End Developer</span>
-                        <q-item-label caption lines='1'>
-                            Suggested by <span class='text-weight-medium'>Jeroen Dewelde</span>
-                        </q-item-label>
-                    </q-item-section>
-
-                    <q-item-section top side>
-                        <div class='text-grey-8 q-gutter-xs'>
-                            <q-btn class='gt-xs' size='12px' flat dense round icon='comment' />
-                            <q-btn class='gt-xs' size='12px' flat dense round icon='info' />
-                            <q-btn class='gt-xs' size='12px' flat dense round icon='delete' />
-                        </div>
-                    </q-item-section>
-                </q-item>
+                        <q-item-section top side>
+                            <div class='text-grey-8 q-gutter-xs'>
+                                <q-btn class='gt-xs' size='12px' flat dense round icon='comment' />
+                                <q-btn class='gt-xs' size='12px' flat dense round icon='info' />
+                                <q-btn class='gt-xs' size='12px' flat dense round icon='delete' />
+                            </div>
+                        </q-item-section>
+                    </q-item>
+                    <q-separator spaced />
+                </div>
                 <!--                <q-separator spaced />-->
             </q-list>
 
@@ -69,8 +62,6 @@
             </q-card-actions>
 
         </q-card>
-
-
 
 
         <q-page-sticky position='bottom-right' :offset='[18, 18]'>
