4f4b48a32913885c6559144e55a359d4a7dff75e
Wouter01 <wouterhennen@gmail.com>
2022-03-19 17:06:24 +0100

    Added mockup data, added colors to chips

--- fe/src/components/appPages/Projects.vue
+++ fe/src/components/appPages/Projects.vue
@@ -7,15 +7,15 @@
         
         
         <div class='row justify-start content-start'>
-            <div v-for='(item, index) in [ {}, {}, {}, {}, {}, {}, {} ]' :key='index' class="col-sm-12 col-md-6 col-lg-4 col-xl-3">
+            <div v-for='project in projects' :key='project.id' class="col-sm-12 col-md-6 col-lg-4 col-xl-3">
             <q-card 
                     class='my-card shadow-4 q-ma-sm' flat bordered>
 
                 <q-card-section>
                     <div class='row'>
                         <div class='col'>
-                            <div class='text-h5 text-bold q-mt-sm q-mb-xs'>Project {{ index }}</div>
-                            <div class='text-overline'>Client {{ index }}</div>
+                            <div class='text-h5 text-bold q-mt-sm q-mb-xs'>{{ project.title }}</div>
+                            <div class='text-overline'>{{ project.client }}</div>
                         </div>
                         <div>
                             <q-btn flat round size="12px" color='primary' icon='mail' />
@@ -24,30 +24,38 @@
                         </div>
                     </div>
                     <div class='text-caption text-grey'>Coaches:</div>
-                    <q-chip outline icon='person'>Astrid</q-chip>
-                    <q-chip outline icon='person'>Miet</q-chip>
+                    <q-chip v-for="coach in project.coaches" :key="coach.id" icon='person'>{{ coach.name }}</q-chip>
 
                     <div class='text-caption text-grey'>Roles:</div>
-                    <q-chip outline icon='info'>
-                        <q-tooltip anchor='top middle' self='center middle'>
-                            Must know everything
-                        </q-tooltip>
-                        1x Full Stack Developer
-                    </q-chip>
-                    <q-chip outline>
-                        1x Comms
-                    </q-chip>
-                    <q-chip outline icon='info'>
-                        <q-tooltip anchor='top middle' self='center middle'>
-                            Must know React.js
-                        </q-tooltip>
-                        1x Front-end Developer
-                    </q-chip>
-                    <q-chip outline>
-                        1x Media
-                    </q-chip>
-                    <q-chip outline>
-                        1x Back-end Developer
+                    <q-chip
+                        outline
+                        :color="role.color + '-4'"
+                        v-for="(role, index) in project.roles" 
+                        :class="'bg-' + role.color + '-1'"
+                        style="border-width: 1.5px;"
+                        :style="'padding-left: ' + (role.info ? '2px' : '8px') + '; padding-right: 8px;'" :key="index">
+                        <template v-slot:default>
+                            <div class="row" style="display: flex; align-items: center">
+                                <q-icon v-if="role.info" 
+                                    name="info" 
+                                    size="sm" 
+                                    :color="role.color + '-6'"/>
+                                <div style="color: black;" 
+                                    :style="'padding-left: ' + (role.info ? '3px' : '0px')"
+                                    >{{ role.label }}</div>
+                                <div class="text-bold" 
+                                    style="padding-left: 3px;" 
+                                    :class="'text-' + role.color + '-8'" 
+                                    >{{ role.amount }}</div>
+                                <q-tooltip 
+                                    v-if="role.info" 
+                                    :class="'bg-' + role.color + '-2'" 
+                                    class="text-black shadow-2" 
+                                    anchor='bottom middle' 
+                                    self='center middle'
+                                    >{{ role.info }}</q-tooltip>
+                            </div>
+                        </template>
                     </q-chip>
 
                 </q-card-section>
@@ -64,9 +72,7 @@
                                     <span class='text-weight-medium'>Dayana Stark</span>
                                 </q-item-label>
                                 <span class='text-grey-8 text-caption'>Frond End Developer</span>
-                                <q-item-label caption lines='1'>
-                                    Suggested by <span class='text-weight-medium'>Jeroen Dewelde</span>
-                                </q-item-label>
+                               
                             </q-item-section>
 
                             <q-item-section top side>
@@ -103,16 +109,34 @@ import SideBar from '../tools/SideBar.vue'
 
 export default {
     name: 'Projects.vue',
-
+    
     components: {
         SideBar,
     },
+    data() {
+        return {
+            projects: [
+                {
+                    id: 1,
+                    title: "Project #1",
+                    client: "Client #1",
+                    coaches: [
+                        { id: 1, name: "Miet" },
+                        { id: 2, name: "Astrid" }
+                    ],
+                    roles: [
+                        { type: "fullstack", amount: 2, label: "Full-Stack dev", color: 'green', info: "Must know everything." },
+                        { type: "comms", amount: 1, label: "Communication", color: 'purple' },
+                        { type: "frontend", amount: 2, label: "Frontend dev", color: 'orange', info: "Must know React.js." },
+                        { type: "media", amount: 2, label: "Media", color: 'red' },
+                        { type: "backend", amount: 2, label: "Backend dev", color: 'blue' }
+                    ]
+                }
+            ]
+        }
+    }
 }
 </script>
-
-<style scoped>
-
-</style>
 
 <style lang='sass' scoped>
 .my-card
