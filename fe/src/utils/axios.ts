0ba2b583ccbb2ea8bdd51e26341f3d4b7af63b21
Wouter01 <wouterhennen@gmail.com>
2022-03-24 22:28:34 +0100

    Added support for server

--- fe/src/utils/axios.ts
+++ fe/src/utils/axios.ts
@@ -18,7 +18,7 @@ function getCookie(name: String) {
 
 export const instance = axios.create({
   withCredentials: true,
-  baseURL: 'http://localhost:8000/api/',
+  baseURL: 'https://sel2-5.ugent.be/api/',
 })
 
 export const setCsrfToken = () =>
