6016bfffbe185841a38ce80caa99dbf3525bb400
Henri Cattoire <henricattoire@yahoo.com>
2022-03-30 19:07:59 +0200

    add getNested.

--- be/osoc/common/utils.py
+++ be/osoc/common/utils.py
@@ -2,6 +2,17 @@
 Utilities.
 """
 
+def getNested(dictionary, default, *keys):
+    """
+    Get a dictionary but nested; returning the default at the first missing key.
+    """
+    val = dictionary
+    for key in keys:
+        val = val.get(key, default)
+        if val == default:
+            return default
+    return val
+
 def strip_and_lower_email(email):
     """
     Strip email and transform it to lowercase.
