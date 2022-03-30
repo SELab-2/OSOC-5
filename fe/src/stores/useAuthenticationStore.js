8c182fa4ec73438cdd9a7f008e6d3293ec676f26
Lisa De Jonghe <lisa.dejonghe@ugent.be>
2022-03-30 19:59:46 +0200

    correct links

--- fe/src/stores/useAuthenticationStore.js
+++ fe/src/stores/useAuthenticationStore.js
@@ -7,7 +7,7 @@ import {convertObjectKeysToCamelCase} from "../utils/case-conversion";
 //   loggedInUser: { email: string; password: string } | undefined
 // }
 
-const host = 'http://localhost:3000'
+const host = 'https://sel2-5.ugent.be'
 
 export const useAuthenticationStore = defineStore('user/authentication', {
   state: () => ({
@@ -17,7 +17,7 @@ export const useAuthenticationStore = defineStore('user/authentication', {
     // https://sel2-5.ugent.be
     // admin@example.com
     async login({ email, password }) {
-      await axios.post('http://localhost:8000/api/login/', {
+      await axios.post('https://sel2-5.ugent.be/api/login/', {
         username: email,
         email,
         password,
