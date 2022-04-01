b378e588906da652c908b63964e6b75125822d99
Lisa De Jonghe <lisa.dejonghe@ugent.be>
2022-04-01 14:51:28 +0200

    keep user as loggedInUser

--- fe/src/stores/useAuthenticationStore.js
+++ fe/src/stores/useAuthenticationStore.js
@@ -2,12 +2,14 @@ import axios from 'axios'
 import { defineStore } from 'pinia'
 import { setCsrfToken } from '../utils/axios'
 import {convertObjectKeysToCamelCase} from "../utils/case-conversion";
+import router from "../router/index"
+
 // 
 // interface State {
 //   loggedInUser: { email: string; password: string } | undefined
 // }
 
-const host = 'https://sel2-5.ugent.be'
+const host = 'http://localhost:3000'
 
 export const useAuthenticationStore = defineStore('user/authentication', {
   state: () => ({
@@ -17,19 +19,16 @@ export const useAuthenticationStore = defineStore('user/authentication', {
     // https://sel2-5.ugent.be
     // admin@example.com
     async login({ email, password }) {
-      await axios.post('https://sel2-5.ugent.be/api/login/', {
+      await axios.post('http://localhost:8000/api/login/', {
         username: email,
         email,
         password,
       },{withCredentials: true}).then(({data}) => {
-        console.log({ email: email, password: password })
         setCsrfToken()
 
-        // this.loggedInUser = convertObjectKeysToCamelCase(data).results // doesn't work yet!
+        this.loggedInUser = convertObjectKeysToCamelCase(data).user // doesn't work yet!
 
-        this.loggedInUser = { email: email, password: password }
-
-        window.location.href = host + '/students'
+        router.push('/students')
       })
     },
     logout() {
