d44009c34af6e71319fb59c5ed825b6ad003024f
Zelzahn <friedrich@friedrichvdb.be>
2022-04-03 09:31:19 +0200

    only coach should be inactive

--- be/osoc/common/signals.py
+++ be/osoc/common/signals.py
@@ -8,9 +8,3 @@ from osoc.common.models import Coach, Student
 def set_new_user_inactive(sender, instance, **kwargs):
     if instance._state.adding is True:
         instance.is_active = False
-
-
-@receiver(pre_save, sender=Student)
-def set_new_user_inactive(sender, instance, **kwargs):
-    if instance._state.adding is True:
-        instance.is_active = False
