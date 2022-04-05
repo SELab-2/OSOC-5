ccbf364a04317957177abee46dc62668ccdb869c
Friedrich <friedrich@friedrichvdb.be>
2022-04-05 12:50:21 +0200

    added class definitions (#161)

--- fe/src/models/Skill.ts
+++ fe/src/models/Skill.ts
@@ -1,4 +1,14 @@
-export interface Skill {
+export interface SkillInterface {
   name: string
   description: string
+}
+
+export class Skill implements SkillInterface {
+  name: string
+  description: string
+
+  constructor(name: string, description: string) {
+    this.name = name
+    this.description = description
+  }
 }
