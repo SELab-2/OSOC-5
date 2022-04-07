ac75adae0b6655784a3b540bd2010250a92bc51f
Friedrich <friedrich@friedrichvdb.be>
2022-04-07 14:03:57 +0200

    Default for coach is now inactive (#140)
    * default for coach and student is now inactive
    
    * only coach should be inactive
    
    * Remove unnecessary import

---
+++ be/osoc/common/signals.py
@@ -0,0 +1,10 @@
+from django.dispatch import receiver
+from django.db.models.signals import pre_save
+
+from osoc.common.models import Coach
+
+
+@receiver(pre_save, sender=Coach)
+def set_new_user_inactive(sender, instance, **kwargs):
+    if instance._state.adding is True:
+        instance.is_active = False
