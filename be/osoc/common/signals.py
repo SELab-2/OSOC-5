6fbd36098aff0950e00b6b5726ed4559edc62a4a
Friedrich <friedrich@friedrichvdb.be>
2022-04-08 11:10:28 +0200

    Be/fix login (#173)
    * name is now dynamic
    
    * only set non-admin users inactive

--- be/osoc/common/signals.py
+++ be/osoc/common/signals.py
@@ -6,5 +6,5 @@ from osoc.common.models import Coach
 
 @receiver(pre_save, sender=Coach)
 def set_new_user_inactive(sender, instance, **kwargs):
-    if instance._state.adding is True:
+    if instance._state.adding is True and not sender.is_admin:
         instance.is_active = False
