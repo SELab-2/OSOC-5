4546556f3644f9f1e29c4d8e602f2f772e728c2a
leveys <lennartveys@gmail.com>
2022-04-08 19:16:46 +0200

    added extra filter functionality

--- be/osoc/common/filters.py
+++ be/osoc/common/filters.py
@@ -1,8 +1,12 @@
 """
 Filters used in views.py
 """
+from django.forms import ValidationError
 from rest_framework import filters
 from .models import Project, Student, Suggestion
+
+true_strings = ['true', '1', 'yes', 't', 'y']
+false_strings = ['false', '0', 'no', 'f', 'n']
 
 class StudentOnProjectFilter(filters.BaseFilterBackend):
     """
@@ -10,14 +14,18 @@ class StudentOnProjectFilter(filters.BaseFilterBackend):
     query parameter 'on_project' should be included in the url
     """
     def filter_queryset(self, request, queryset, view):
-        if request.query_params.get('on_project') is not None:
+        param = request.query_params.get('on_project')
+        if param is not None:
             suggested_students = Student.objects.none()
             for project in Project.objects.all():
                 suggested_students |= project.suggested_students.all()  # union
 
             # subquery is needed because filtering is not possible on a union of queries for some reason
             subquery = suggested_students.distinct()
-            return queryset.filter(id__in=subquery.values('id'))
+            if param.lower() in true_strings:
+                return queryset.filter(id__in=subquery.values('id'))
+            elif param.lower() in false_strings:
+                return queryset.exclude(id__in=subquery.values('id'))
         return queryset
 
 
@@ -27,8 +35,12 @@ class StudentSuggestedByUserFilter(filters.BaseFilterBackend):
     query parameter 'suggested_by_user' should be included in the url
     """
     def filter_queryset(self, request, queryset, view):
-        if request.query_params.get('suggested_by_user') is not None:
-            return queryset.filter(suggestions__id=request.user.id)
+        param = request.query_params.get('suggested_by_user')
+        if param is not None:
+            if param.lower() in true_strings:
+                return queryset.filter(suggestions__id=request.user.id)
+            elif param.lower() in false_strings:
+                return queryset.exclude(suggestions__id=request.user.id)
         return queryset
 
 
@@ -47,9 +59,9 @@ class StudentFinalDecisionFilter(filters.BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
         param = request.query_params.get('suggestion')
         if param is not None:
-            if param in self.param2enum.keys():
+            if param.lower() in self.param2enum.keys():
                 return queryset.filter(final_decision__suggestion=self.param2enum[param])
-            if param in ['none', '3']:
+            if param.lower() in ['none', '3']:
                 return queryset.filter(final_decision=None)
         return queryset
 
@@ -60,10 +72,14 @@ class EmailDateTimeFilter(filters.BaseFilterBackend):
     query parameter should be one of: ['?date=', '?before=', '?after=']
     """
     def filter_queryset(self, request, queryset, view):
-        if (date := request.query_params.get('date')) is not None:
-            return queryset.filter(time__date=date)
-        if (before := request.query_params.get('before')) is not None:
-            queryset = queryset.filter(time__lt=before)
-        if (after := request.query_params.get('after')) is not None:
-            queryset = queryset.filter(time__gt=after)
+        try:
+            if (date := request.query_params.get('date')) is not None:
+                return queryset.filter(time__date=date)
+            if (before := request.query_params.get('before')) is not None:
+                queryset = queryset.filter(time__lt=before)
+            if (after := request.query_params.get('after')) is not None:
+                queryset = queryset.filter(time__gt=after)
+        except ValidationError:
+            # return default queryset when a validationerror is raised
+            pass
         return queryset
--- be/osoc/common/views.py
+++ be/osoc/common/views.py
@@ -23,9 +23,15 @@ from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 class StudentViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows students to be viewed, edited or searched.
-    Search students with the query parameter '?search='
-    Filter students with the query parameters '?alum=', '?language=', '?skills=', '?on_project', '?suggested_by_user' and '?suggestion='
-    example query: /api/students/?alum=true&language=0&skills=1&suggestion=yes&on_project
+    Search students with the query parameter ?search=
+    Filter students with the query parameters:
+        ?alum=[true, false],
+        ?language=[0-4],
+        ?skills=:id:,
+        ?on_project=[true, false],
+        ?suggested_by_user=[true, false],
+        ?suggestion=[yes, no, maybe, none, 0, 1, 2, 3]
+    example query: /api/students/?alum=true&language=0&skills=1&suggestion=yes&on_project=true
     """
     queryset = Student.objects.all().order_by('id')
     serializer_class = StudentSerializer
@@ -130,8 +136,10 @@ class CoachViewSet(viewsets.GenericViewSet,
     API endpoint that allows coaches to be viewed, edited or searched.
     a coach cannot be created by this API endpoint
     a coach can only update and view its own data, except for admins
-    Search coaches with the query parameter '?search='
-    Filter coaches with the query parameters '?is_admin=' and '?is_active='
+    Search coaches with the query parameter ?search=
+    Filter coaches with the query parameters 
+        ?is_admin=[true, false],
+        ?is_active=[true, false]
     example query: /api/coaches/?is_admin=false&is_active=true
     """
     queryset = Coach.objects.all().order_by('id')
@@ -181,8 +189,11 @@ class ProjectViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows projects to be viewed, edited or searched.
     only admin users have permission for this endpoint, except for suggesting students or removing suggestions 
-    Search projects with the query parameter '?search='
-    Filter projects with the query parameters '?required_skills=', '?coaches=' and '?suggested_students='
+    Search projects with the query parameter ?search=
+    Filter projects with the query parameters 
+        ?required_skills=:id:, 
+        ?coaches=:id:,
+        ?suggested_students=:id:
     example query: /api/projects/?required_skills=1&coaches=2&suggested_students=1
     """
     queryset = Project.objects.all().order_by('id')
@@ -281,7 +292,7 @@ class ProjectViewSet(viewsets.ModelViewSet):
 class SkillViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows skills to be viewed, edited or searched.
-    Search skills with the query parameter '?search='
+    Search skills with the query parameter ?search=
     """
     queryset = Skill.objects.all().order_by('id')
     serializer_class = SkillSerializer
@@ -293,8 +304,13 @@ class SkillViewSet(viewsets.ModelViewSet):
 class SentEmailViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows sent emails to be viewed, edited or searched.
-    Search emails with the query parameter '?search='
-    Filter emails with the query parameters '?sender=', '?receiver=', '?date=', '?before=' and '?after='
+    Search emails with the query parameter ?search=
+    Filter emails with the query parameters 
+        ?sender=:id:, 
+        ?receiver=:id:, 
+        ?date=yyyy-mm-dd, 
+        ?before=yyyy-mm-ddThh:mm:ss,
+        ?after=yyyy-mm-ddThh:mm:ss
     example query: /api/emails/?sender=1&after=2022-04-03
     """
     queryset = SentEmail.objects.all().order_by('id')
