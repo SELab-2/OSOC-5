5a67f55be803a7cf440b0ae5b48fcf64fb9d000e
Lisa De Jonghe <lisa.dejonghe@ugent.be>
2022-04-12 20:15:05 +0200

    add extra fields to student

--- fe/src/models/Student.ts
+++ fe/src/models/Student.ts
@@ -17,7 +17,7 @@ export interface StudentInterface {
   email: string
   phoneNumber: string
   language: Language
-  extraInfo: string
+  motivation: string
   cv: URL
   portfolio: URL
   lastEmailSent: Date
@@ -28,6 +28,16 @@ export interface StudentInterface {
   finalDecision: Suggestion | null
   suggestions: Array<Suggestion>
   alum: boolean
+  bestSkill: Skill | string
+  degreeCurrentYear: number
+  degreeDuration: number
+  employmentAgreement: string
+  englishRating: number
+  funFact: string
+  gender: number
+  hinderWork: string
+  pronouns: string
+  studentCoach: boolean
 }
 
 enum Language {
@@ -47,7 +57,7 @@ export class Student implements StudentInterface {
   email: string
   phoneNumber: string
   language: Language
-  extraInfo: string
+  motivation: string
   cv: URL
   portfolio: URL
   lastEmailSent: Date
@@ -58,6 +68,16 @@ export class Student implements StudentInterface {
   finalDecision: Suggestion | null
   suggestions: Suggestion[]
   alum: boolean
+  bestSkill: Skill | string
+  degreeCurrentYear: number
+  degreeDuration: number
+  employmentAgreement: string
+  englishRating: number
+  funFact: string
+  gender: number
+  hinderWork: string
+  pronouns: string
+  studentCoach: boolean
 
   constructor(obj: StudentInterface);
   constructor(
@@ -69,7 +89,7 @@ export class Student implements StudentInterface {
     email: string,
     phoneNumber: string,
     language: Language,
-    extraInfo: string,
+    motivation: string,
     cv: URL,
     portfolio: URL,
     lastEmailSent: Date,
@@ -79,7 +99,17 @@ export class Student implements StudentInterface {
     skills: Skill[],
     finalDecision: Suggestion | null,
     suggestions: Suggestion[],
-    alum: boolean
+    alum: boolean,
+    bestSkill: Skill | string,
+    degreeCurrentYear: number,
+    degreeDuration: number,
+    employmentAgreement: string,
+    englishRating: number,
+    funFact: string,
+    gender: number,
+    hinderWork: string,
+    pronouns: string,
+    studentCoach: boolean
   );
 
   constructor (...args: any[]) {
@@ -94,7 +124,7 @@ export class Student implements StudentInterface {
       this.email = args[5]
       this.phoneNumber = args[6]
       this.language = args[7]
-      this.extraInfo = args[8]
+      this.motivation = args[8]
       this.cv = args[9]
       this.portfolio = args[10]
       this.lastEmailSent = args[11]
@@ -105,6 +135,16 @@ export class Student implements StudentInterface {
       this.finalDecision = args[16]
       this.suggestions = args[17]
       this.alum = args[18]
+      this.bestSkill = args[19]
+      this.degreeCurrentYear = args[20]
+      this.degreeDuration = args[21]
+      this.employmentAgreement = args[22]
+      this.englishRating = args[23]
+      this.funFact = args[24]
+      this.gender = args[25]
+      this.hinderWork = args[26]
+      this.pronouns = args[27]
+      this.studentCoach = args[28]
     }
   }
 }
