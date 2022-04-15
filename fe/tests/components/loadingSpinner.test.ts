823369ec816a23bb5071677cb58b60f8bfbc2ec8
Lander Saerens <99676075+lasaeren@users.noreply.github.com>
2022-04-15 17:34:31 +0200

    switch from Jest to Vuetest (#223)
    * remove previous test framework
    
    * add vitest
    
    * add test and coverage scripts
    
    * edit vite config to support tests, add jsdom
    
    * disable log of spinner, add skillstore test
    
    * add pinia testing, and show how to add it for component testing
    
    * test to it

---
+++ fe/tests/components/loadingSpinner.test.ts
@@ -0,0 +1,32 @@
+import {test, expect, describe, beforeEach, vi, it} from 'vitest'
+import {mount} from '@vue/test-utils'
+import {Quasar} from 'quasar'
+import Component from '../../src/components/LoadingSpinner.vue'
+import {createPinia, setActivePinia} from "pinia";
+import {createTestingPinia} from '@pinia/testing'
+
+// This makes sure Quasar is loaded
+const wrapperFactory = () => mount(Component, {
+    global: {
+        plugins: [
+            createTestingPinia({
+                createSpy: vi.fn,
+            }),
+            Quasar
+        ]
+    },
+})
+
+describe('example test typescript', () => {
+    // this makes sure Pinia is loaded before every test
+    beforeEach(() => {
+        setActivePinia(createPinia())
+    })
+    // example test
+    it('mount component', () => {
+        expect(Component).toBeTruthy();
+        const wrapper = wrapperFactory();
+
+        // console.log(wrapper.html());
+    })
+})
