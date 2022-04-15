0fe0741c43dd91f24bfab06b6466fad083f4c3c1
lasaeren <lander.saerens@ugent.be>
2022-04-15 16:01:38 +0200

    edit vite config to support tests, add jsdom

---
+++ fe/tests/components/loadingSpinner.test.ts
@@ -0,0 +1,26 @@
+import {test, expect, describe, beforeEach} from 'vitest'
+import {mount} from '@vue/test-utils'
+import {Quasar} from 'quasar'
+import Component from '../../src/components/LoadingSpinner.vue'
+import {createPinia, setActivePinia} from "pinia";
+
+// This makes sure Quasar is loaded
+const wrapperFactory = () => mount(Component, {
+    global: {
+        plugins: [Quasar]
+    },
+})
+
+describe('example test typescript', () => {
+    // this makes sure Pinia is loaded before every test
+    beforeEach(() => {
+        setActivePinia(createPinia())
+    })
+    // example test
+    test('mount component', () => {
+        expect(Component).toBeTruthy();
+        const wrapper = wrapperFactory();
+
+        console.log(wrapper.html());
+    })
+})
