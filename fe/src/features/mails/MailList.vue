0c1972101737c555b26bb5a3d81c2bd9ca89ad29
Lisa De Jonghe <lisa.dejonghe@ugent.be>
2022-04-18 12:15:48 +0200

    beginning of mail list

--- fe/src/features/mails/MailList.vue
+++ fe/src/features/mails/MailList.vue
@@ -26,22 +26,60 @@
         </q-input>
       </div>
     </div>
+
+    <q-table
+      class="my-table mail-table shadow-4"
+      :rows="studentStore.students"
+      :columns="columns"
+      row-key="id"
+      separator="horizontal"
+    >
+    </q-table>
   </div>
 </template>
 
 <script lang="ts">
 import {defineComponent} from "@vue/runtime-core";
 import {ref} from 'vue'
+import { Student } from "../../models/Student";
+import { useStudentStore } from "../../stores/useStudentStore";
+
+const columns = [
+  {
+    name: 'name',
+    required: true,
+    label: 'Name',
+    align: 'left' as const,
+    field: (row: Student) => row.firstName + ' ' + row.lastName,
+    sortable: true,
+  },
+  {
+    name: 'email',
+    align: 'right' as const,
+    label: 'Email',
+    field: 'email',
+    sortable: true,
+  },
+]
 
 export default defineComponent({
   setup() {
+    const studentStore = useStudentStore()
+
     return {
-      filter: ref('')
+      studentStore,
+      filter: ref(''),
+      columns
     }
+  },
+  created() {
+    this.studentStore.loadStudents()
   }
 })
 </script>
 
 <style scoped>
-
+.mail-table {
+  border-radius: 10px;
+}
 </style>
