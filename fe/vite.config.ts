128e6a196590b898cb9ca7e58beac56da87dae7a
Lander Saerens <99676075+lasaeren@users.noreply.github.com>
2022-04-18 13:57:45 +0200

    fix setup of vitest (#242)

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  test: {
-    environment: 'jsdom'
-  },
   plugins: [
     vue({
       template: { transformAssetUrls }
