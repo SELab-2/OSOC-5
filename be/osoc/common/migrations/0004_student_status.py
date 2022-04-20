46a671bfd6ea0b532c7eee25b6d020332d99aba3
Lisa De Jonghe <lisa.dejonghe@ugent.be>
2022-04-20 16:13:53 +0200

    change migrations

--- be/osoc/common/migrations/0004_student_status.py
+++ be/osoc/common/migrations/0004_student_status.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('common', '0002_alter_student_gender'),
+        ('common', '0003_alter_projectsuggestion_unique_together'),
     ]
 
     operations = [
