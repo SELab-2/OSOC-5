eeadea07dc6af17fbcba0677cae012b5427ef457
Zelzahn <friedrich@friedrichvdb.be>
2022-05-01 14:48:04 +0200

    docstrings

--- be/osoc/common/consumers.py
+++ be/osoc/common/consumers.py
@@ -1,10 +1,21 @@
+"""
+websocket consumers are defined here
+"""
+
 import json
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
 
 
 class CoachConsumer(WebsocketConsumer):
+    """
+    CoachConsumer is a consumer linked with the suggestions made by a coach
+    """
+
     def connect(self):
+        """
+        called when a client connects to the websocket
+        """
         async_to_sync(self.channel_layer.group_add)(
             "suggestion",
             self.channel_name
@@ -13,12 +24,18 @@ class CoachConsumer(WebsocketConsumer):
         self.accept()
 
     def disconnect(self, _):
+        """
+        called when a client disconnects to the websocket
+        """
         async_to_sync(self.channel_layer.group_discard(
             "suggestion",
             self.channel_name
         ))
 
     def suggestion(self, event):
+        """
+        a plain suggestion made by a coach
+        """
         suggestion = event['data']
 
         self.send(text_data=json.dumps({
@@ -26,6 +43,9 @@ class CoachConsumer(WebsocketConsumer):
         }))
 
     def remove_suggestion(self, event):
+        """
+        when a choach changed the suggestion to: "not decided"
+        """
         suggestion = event['data']
 
         self.send(text_data=json.dumps({
@@ -33,6 +53,9 @@ class CoachConsumer(WebsocketConsumer):
         }))
 
     def final_decision(self, event):
+        """
+        a final decision made by an admin
+        """
         suggestion = event['data']
 
         self.send(text_data=json.dumps({
@@ -40,6 +63,9 @@ class CoachConsumer(WebsocketConsumer):
         }))
 
     def remove_final_decision(self, event):
+        """
+        when an admin changed the suggestion to: "not decided"
+        """
         suggestion = event['data']
 
         self.send(text_data=json.dumps({
@@ -47,6 +73,9 @@ class CoachConsumer(WebsocketConsumer):
         }))
 
     def suggest_student(self, event):
+        """
+        the student is assigned to a project
+        """
         suggestion = event['data']
 
         self.send(text_data=json.dumps({
@@ -54,6 +83,9 @@ class CoachConsumer(WebsocketConsumer):
         }))
 
     def remove_student(self, event):
+        """
+        the student is removed from the project
+        """
         suggestion = event['data']
 
         self.send(text_data=json.dumps({
