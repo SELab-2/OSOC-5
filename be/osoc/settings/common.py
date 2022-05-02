2749edf6dbe9bfaf7061afe5c7aaa67aa23b7dce
Henri Cattoire <henricattoire@yahoo.com>
2022-05-02 15:31:44 +0200

    reference pagination.

--- be/osoc/settings/common.py
+++ be/osoc/settings/common.py
@@ -47,9 +47,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# See pagination.py!
 # REST_FRAMEWORK = {
 #     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-#     'PAGE_SIZE': 2
+#     'PAGE_SIZE': 10
 # }
 
 ROOT_URLCONF = 'osoc.urls'
