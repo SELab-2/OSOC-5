eff9c8c50a0f90cfb07323dc0cef0b1706da1938
lasaeren <lander.saerens@ugent.be>
2022-05-05 09:28:34 +0200

    deleting skill does not have to redirect

--- fe/src/features/projects/components/subcomponents/DeleteSkillDialog.vue
+++ fe/src/features/projects/components/subcomponents/DeleteSkillDialog.vue
@@ -34,8 +34,8 @@
         flat
         color="red"
         label="Delete"
-        @click="deleteSkillConfirm(deleteSkillId ?? -1)"
         glow-color="red-2"
+        @click="deleteSkillConfirm(deleteSkillId ?? -1)"
       />
     </q-card-actions>
   </q-card>
@@ -44,7 +44,6 @@
 <script lang="ts">
 import {defineComponent} from "@vue/runtime-core";
 import {useSkillStore} from "../../../../stores/useSkillStore";
-import router from "../../../../router";
 
 export default defineComponent ({
   props: {
@@ -70,7 +69,6 @@ export default defineComponent ({
         this.skillStore.deleteSkill(id, // callback
           (success: boolean) => {
             if (success) {
-              router.push('/projects')
               this.$q.notify({
                 icon: 'done',
                 color: 'positive',
