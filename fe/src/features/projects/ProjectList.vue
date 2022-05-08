5b2e34ef64aed42eaa0b484309a5d05c4795df9b
Wouter01 <wouterhennen@gmail.com>
2022-05-09 00:26:12 +0200

    reformat projectlist

--- fe/src/features/projects/ProjectList.vue
+++ fe/src/features/projects/ProjectList.vue
@@ -1,63 +1,60 @@
 <template>
+  <div style="overflow: hidden" class="fit column">
     <div
-      style="overflow: hidden;"
-      class="fit column"
+      :class="`${showShadow ? 'shadow-2' : ''}`"
+      style="z-index: 1; transition: box-shadow ease 500ms"
     >
-    <div :class="`${showShadow ? 'shadow-2' : ''}`" style="z-index: 1; transition: box-shadow ease 500ms">
       <q-toolbar
-        style="overflow: visible; padding: 8px; "
+        style="overflow: visible; padding: 8px"
         class="text-blue bg-white"
       >
-        <div class="text-bold text-h4 q-ml-md text-black">
-          Projects
-        </div>
+        <div class="text-bold text-h4 q-ml-md text-black">Projects</div>
         <q-space />
-          
-          <btn
-            round
-            style="margin-top: 9px"
-            size="12px"
-            glow-color="teal-3"
-            shadow-color="teal"
-            :shadow-strength="showFilters ? 2 : 5"
-            :color="showFilters ? 'teal' : 'white'"
-            :class="`text-${showFilters ? 'white' : 'teal'}`"
-            icon="tune"
-            @click="showFilters = !showFilters"
-          />
-          <!-- Do not remove the label attribute, otherwise the label slot does not work -->
-          <q-input
-          
+
+        <btn
+          round
+          style="margin-top: 9px"
+          size="12px"
+          glow-color="teal-3"
+          shadow-color="teal"
+          :shadow-strength="showFilters ? 2 : 5"
+          :color="showFilters ? 'teal' : 'white'"
+          :class="`text-${showFilters ? 'white' : 'teal'}`"
+          icon="tune"
+          @click="showFilters = !showFilters"
+        />
+        <!-- Do not remove the label attribute, otherwise the label slot does not work -->
+        <q-input
           tabindex="-1"
-            v-model="projectNameFilter"
-            debounce="300"
-            dense
-            outlined
-            color="teal"
-            label=""
-            style="margin-top: 5px"
-            hide-bottom-space
-          >
+          v-model="projectNameFilter"
+          debounce="300"
+          dense
+          outlined
+          color="teal"
+          label=""
+          style="margin-top: 5px"
+          hide-bottom-space
+        >
           <template v-slot:label>
             <span class="text-weight-medium text-teal-3">Search Projects</span>
           </template>
           <template v-slot:append>
             <q-icon name="search" color="teal-3" />
           </template>
-          </q-input>
-          <btn
-            round
-            style="margin-top: 9px"
-            size="12px"
-            :icon="expanded ? 'unfold_less' : 'unfold_more'"
-            glow-color="teal-3"
-            shadow-color="teal"
-            :shadow-strength="expanded ? 2 : 5"
-            :color="expanded ? 'teal' : 'white'"
-            :class="`text-${expanded ? 'white' : 'teal'}`"
-            @click="expanded = !expanded"
-          />
-         
+        </q-input>
+        <btn
+          round
+          style="margin-top: 9px"
+          size="12px"
+          :icon="expanded ? 'unfold_less' : 'unfold_more'"
+          glow-color="teal-3"
+          shadow-color="teal"
+          :shadow-strength="expanded ? 2 : 5"
+          :color="expanded ? 'teal' : 'white'"
+          :class="`text-${expanded ? 'white' : 'teal'}`"
+          @click="expanded = !expanded"
+        />
+
         <q-space />
         <btn
           padding="7px"
@@ -68,94 +65,97 @@
           shadow-strength="2"
           no-wrap
         >
-        <div class="ellipsis">Conflicts</div>
+          <div class="ellipsis">Conflicts</div>
         </btn>
       </q-toolbar>
-      
-      <q-slide-transition style="height: fit-content" >
+
+      <q-slide-transition style="height: fit-content">
         <div v-if="showFilters">
           <q-card-section>
-            <span class="text-h5 text-bold">Filters</span><br/>
+            <span class="text-h5 text-bold">Filters</span><br />
             <div class="row">
-            <div class="column">
-            <q-checkbox  label="My project"/>
-            <q-checkbox  label="Students needed"/>
-            </div>
-            <div>
-            <q-select
-              v-model="studentStore.skills"
-              rounded
-              outlined
-              dense
-              multiple
-              color="primary"
-              bg-color="white"
-              :options="skillStore.skills"
-              :option-label="opt => opt.name"
-              :option-value="opt => opt.id"
-              label="Skills"
-              style="width: 200px"
-            >
-              <template #selected>
-                <div
-                  class="full-width"
-                  style="max-height: 15vh; overflow-y: auto"
+              <div class="column">
+                <q-checkbox label="My project" />
+                <q-checkbox label="Students needed" />
+              </div>
+              <div>
+                <q-select
+                  v-model="studentStore.skills"
+                  rounded
+                  outlined
+                  dense
+                  multiple
+                  color="primary"
+                  bg-color="white"
+                  :options="skillStore.skills"
+                  :option-label="(opt) => opt.name"
+                  :option-value="(opt) => opt.id"
+                  label="Skills"
+                  style="width: 200px"
                 >
-                  <StudentSkillChip
-                    v-for="skill of studentStore.skills"
-                    :key="skill.id"
-                    :color="skill.color"
-                    :name="skill.name"
-                    best-skill=""
-                  />
-                </div>
-              </template>
-            </q-select>
-          </div>
+                  <template #selected>
+                    <div
+                      class="full-width"
+                      style="max-height: 15vh; overflow-y: auto"
+                    >
+                      <StudentSkillChip
+                        v-for="skill of studentStore.skills"
+                        :key="skill.id"
+                        :color="skill.color"
+                        :name="skill.name"
+                        best-skill=""
+                      />
+                    </div>
+                  </template>
+                </q-select>
+              </div>
             </div>
           </q-card-section>
         </div>
       </q-slide-transition>
     </div>
-    
-    <div id="scroll-target-id" style="flex:1; overflow: auto; " @scroll="showShadow = $event.target.scrollTop > 5">
-      <q-infinite-scroll ref="infinite" @load="(i,done) => projectStore.loadNext(i, done, filters)" :offset="250"  scroll-target="#scroll-target-id" >
-        
-      <masonry-wall
-        
-        :items="projectStore.projects"
-        :ssr-columns="1"
-        :column-width="320"
-        :gap="0"
-        
+
+    <div
+      id="scroll-target-id"
+      style="flex: 1; overflow: auto"
+      @scroll="showShadow = $event.target.scrollTop > 5"
+    >
+      <q-infinite-scroll
+        ref="infinite"
+        @load="(i, done) => projectStore.loadNext(i, done, filters)"
+        :offset="250"
+        scroll-target="#scroll-target-id"
       >
-        <template #default="{ item }">
-          <project-card :project="item" />
-        </template>
-      </masonry-wall>
+        <masonry-wall
+          :items="projectStore.projects"
+          :ssr-columns="1"
+          :column-width="320"
+          :gap="0"
+        >
+          <template #default="{ item }">
+            <project-card :project="item" />
+          </template>
+        </masonry-wall>
         <template v-slot:loading>
           <div class="row justify-center q-my-md">
             <q-spinner color="teal" size="40px" />
           </div>
         </template>
       </q-infinite-scroll>
-      </div>
     </div>
+  </div>
 
-    <q-page-sticky
-      position="bottom-right"
-      :offset="[18, 18]"
-    >
-      <btn
-        fab
-        v-if="authenticationStore.loggedInUser?.isAdmin"
-        padding="10px"
-        icon="add"
-        color="yellow"
-        to="/projects/create"
-        shadow-color="orange"
-      />
-    </q-page-sticky>
+  <q-page-sticky position="bottom-right" :offset="[18, 18]">
+    <btn
+      fab
+      v-if="authenticationStore.loggedInUser?.isAdmin"
+      padding="10px"
+      icon="add"
+      color="yellow"
+      to="/projects/create"
+      shadow-color="orange"
+    />
+  </q-page-sticky>
 </template>
 
 <script lang="ts">
@@ -163,24 +163,24 @@ import { ref, defineComponent } from 'vue'
 import SideBar from '../../components/SideBar.vue'
 import ProjectCard from './components/ProjectCard.vue'
 import { useProjectStore } from '../../stores/useProjectStore'
-import { useStudentStore } from "../../stores/useStudentStore";
+import { useStudentStore } from '../../stores/useStudentStore'
 import { useAuthenticationStore } from '../../stores/useAuthenticationStore'
-import { useSkillStore } from '../../stores/useSkillStore';
+import { useSkillStore } from '../../stores/useSkillStore'
 
 export default defineComponent({
   name: 'ProjectList',
   components: { SideBar, ProjectCard },
   setup() {
-  const baseURL =
-    process.env.NODE_ENV == 'development'
-      ? 'ws://localhost:8000/ws/socket_server/'
-      : 'wss://sel2-5.ugent.be/ws/socket_server/'
+    const baseURL =
+      process.env.NODE_ENV == 'development'
+        ? 'ws://localhost:8000/ws/socket_server/'
+        : 'wss://sel2-5.ugent.be/ws/socket_server/'
     return {
       projectStore: useProjectStore(),
       studentStore: useStudentStore(),
       authenticationStore: useAuthenticationStore(),
       skillStore: useSkillStore(),
-      socket: new WebSocket(baseURL)
+      socket: new WebSocket(baseURL),
     }
   },
   data() {
@@ -193,14 +193,17 @@ export default defineComponent({
   computed: {
     filters() {
       return {
-        search: this.projectNameFilter
+        search: this.projectNameFilter,
       }
     },
     expanded: {
       get() {
         if (this.projectStore.projects.length === 0) return false
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        return (this as any).projectStore.projects.every((p: { selectedRoles: any; }) => Object.values(p.selectedRoles ?? {k:false}).every(r => r))
+        return (this as any).projectStore.projects.every(
+          (p: { selectedRoles: any }) =>
+            Object.values(p.selectedRoles ?? { k: false }).every((r) => r)
+        )
       },
       set(newValue) {
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -209,8 +212,8 @@ export default defineComponent({
             p.selectedRoles[r] = newValue
           }
         })
-      }
-    }
+      },
+    },
   },
   watch: {
     projectNameFilter: {
@@ -218,31 +221,27 @@ export default defineComponent({
         this.$refs.infinite.reset()
         this.$refs.infinite.resume()
         this.$refs.infinite.trigger()
-      }
-    }
+      },
+    },
   },
   mounted() {
-      this.socket.onmessage = async (event: { data: string }) => {
-          const data = JSON.parse(event.data)
+    this.socket.onmessage = async (event: { data: string }) => {
+      const data = JSON.parse(event.data)
 
-          if(data.hasOwnProperty('suggestion')) {
-            await this.studentStore.receiveSuggestion(data.suggestion)
-          }
-          else if(data.hasOwnProperty('remove_suggestion')) {
-            this.studentStore.removeSuggestion(data.remove_suggestion)
-          }
-          else if(data.hasOwnProperty('final_decision')) {
-            this.studentStore.receiveFinalDecision(data.final_decision)
-          }
-          else if(data.hasOwnProperty('remove_final_decision')) {
-            this.studentStore.removeFinalDecision(data.remove_final_decision)
-          }
-          else if(data.hasOwnProperty('suggest_student'))
-            this.projectStore.receiveSuggestion(data.suggest_student)
-          else if(data.hasOwnProperty('remove_student'))
-            this.projectStore.removeReceivedSuggestion(data.remove_student)
-      }
-  }
+      if (data.hasOwnProperty('suggestion')) {
+        await this.studentStore.receiveSuggestion(data.suggestion)
+      } else if (data.hasOwnProperty('remove_suggestion')) {
+        this.studentStore.removeSuggestion(data.remove_suggestion)
+      } else if (data.hasOwnProperty('final_decision')) {
+        this.studentStore.receiveFinalDecision(data.final_decision)
+      } else if (data.hasOwnProperty('remove_final_decision')) {
+        this.studentStore.removeFinalDecision(data.remove_final_decision)
+      } else if (data.hasOwnProperty('suggest_student'))
+        this.projectStore.receiveSuggestion(data.suggest_student)
+      else if (data.hasOwnProperty('remove_student'))
+        this.projectStore.removeReceivedSuggestion(data.remove_student)
+    }
+  },
 })
 </script>
 
@@ -261,5 +260,4 @@ export default defineComponent({
 
 .q-btn
     margin: 5px
-    
 </style>
