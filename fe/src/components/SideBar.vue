c6c0fd3811d38e2ff8d085b42e6261060c87f7fb
Lisa De Jonghe <lisa.dejonghe@ugent.be>
2022-05-11 12:05:23 +0200

    fix skills filter and remove inner loading

--- fe/src/components/SideBar.vue
+++ fe/src/components/SideBar.vue
@@ -8,17 +8,17 @@
       :breakpoint="100"
       class="bg-grey-1 shadow-4"
     >
-    <div
-    :style="!miniState? '' : 'display: none'"
-      style="height: 100%; overflow: hidden;"
-      class="fit column"
-    >
-        
-          <div :class="`${showShadow ? 'shadow-2' : ''}`" style="z-index: 1; transition: box-shadow ease 500ms" class="q-px-sm">
-            <div class="text-bold text-h5 q-py-sm">
-              Students
-            </div>
-            <div class="row no-wrap q-pb-sm">
+      <div
+        :style="!miniState? '' : 'display: none'"
+        style="height: 100%; overflow: hidden;"
+        class="fit column"
+      >
+        <div :class="`${showShadow ? 'shadow-2' : ''}`" style="z-index: 1; transition: box-shadow ease 500ms"
+             class="q-px-sm">
+          <div class="text-bold text-h5 q-py-sm">
+            Students
+          </div>
+          <div class="row no-wrap q-pb-sm">
             <q-input
               v-model="search"
               @update:modelValue="async () => await loadStudents($refs.infiniteScroll)"
@@ -30,9 +30,9 @@
               label="Search Students"
               hide-bottom-space
             >
-            <template v-slot:append>
-              <q-icon name="search" color="teal-4" />
-            </template>
+              <template v-slot:append>
+                <q-icon name="search" color="teal-4"/>
+              </template>
             </q-input>
             <btn
               round
@@ -45,164 +45,162 @@
               icon="tune"
               @click="showFilters = !showFilters"
             />
-            
+
           </div>
-            <q-slide-transition>
-              <div v-if="showFilters" class="overflow-hidden">
-                <!-- div needs to be wrapped because gutter produces negative margins, which cause issues with q-slide-transition -->
-                <div class="q-gutter-y-sm q-px-xs">
-                
-            <SegmentedControl
-              v-model="alumni"
-              color="primary"
-              text-color="white"
-              class="q-mt-md"
-              :options="[
+          <q-slide-transition>
+            <div v-if="showFilters" class="overflow-hidden">
+              <!-- div needs to be wrapped because gutter produces negative margins, which cause issues with q-slide-transition -->
+              <div class="q-gutter-y-sm q-px-xs">
+
+                <SegmentedControl
+                  v-model="alumni"
+                  color="primary"
+                  text-color="white"
+                  class="q-mt-md"
+                  :options="[
                 { name: 'all', label: 'All' },
                 { name: 'alumni', label: 'Alumni' },
                 { name: 'student coaches', label: 'Student Coaches'}
               ]"
-              @click="async () => await loadStudents($refs.infiniteScroll)"
-            />
+                  @click="async () => await loadStudents($refs.infiniteScroll)"
+                />
 
-            <label>Suggestion:</label>
-            <SegmentedControl
-              v-model="suggestion"
-              color="primary"
-              :options="[
+                <label>Suggestion:</label>
+                <SegmentedControl
+                  v-model="suggestion"
+                  color="primary"
+                  :options="[
                 { name: 'yes', label: 'Yes' },
                 { name: 'maybe', label: 'Maybe' },
                 { name: 'no', label: 'No' },
                 { name: 'none', label: 'None' },
               ]"
-              @click="async () => await loadStudents($refs.infiniteScroll)"
-            />
+                  @click="async () => await loadStudents($refs.infiniteScroll)"
+                />
 
-            <q-select
-              v-model="skills"
-              rounded
-              outlined
-              dense
-              multiple
-              color="primary"
-              bg-color="white"
-              :options="skillStore.skills"
-              :option-label="opt => opt.name"
-              :option-value="opt => opt.id"
-              label="Skills"
-              @update:model-value="async () => await loadStudents($refs.infiniteScroll)"
-            >
-              <template #selected>
-                <div
-                  class="full-width"
-                  style="max-height: 15vh; overflow-y: auto"
+                <q-select
+                  v-model="skills"
+                  clearable
+                  rounded
+                  outlined
+                  dense
+                  multiple
+                  color="primary"
+                  bg-color="white"
+                  :options="skillStore.skills"
+                  :option-label="opt => opt.name"
+                  :option-value="opt => opt.id"
+                  label="Skills"
+                  @update:model-value="async () => await loadStudents($refs.infiniteScroll)"
                 >
-                  <StudentSkillChip
-                    v-for="skill of studentStore.skills"
-                    :key="skill.id"
-                    :color="skill.color"
-                    :name="skill.name"
-                    best-skill=""
+                  <template #selected>
+                    <div
+                      class="full-width"
+                      style="max-height: 15vh; overflow-y: auto"
+                    >
+                      <StudentSkillChip
+                        v-for="skill of skills"
+                        :key="skill.id"
+                        :color="skill.color"
+                        :name="skill.name"
+                        best-skill=""
+                      />
+                    </div>
+                  </template>
+                </q-select>
+
+                <q-select
+                  v-model="status"
+                  rounded
+                  outlined
+                  dense
+                  clearable
+                  color="primary"
+                  bg-color="white"
+                  :options="stati"
+                  :option-label="opt => opt.label"
+                  :option-value="opt => opt.value"
+                  label="Status"
+                  emit-value
+                  map-options
+                  @update:model-value="async () => await loadStudents($refs.infiniteScroll)"
+                />
+
+                <div class="row q-gutter-x-md">
+                  <q-checkbox
+                    v-model="byMe"
+                    toggle-indeterminate
+                    false-value="maybe"
+                    indeterminate-value="false"
+                    toggle-order="tf"
+                    color="primary"
+                    label="Suggested by you"
+                    right-label
+                    @click="async () => await loadStudents($refs.infiniteScroll)"
+                  />
+                  <q-checkbox
+                    v-model="onProject"
+                    toggle-indeterminate
+                    true-value="true"
+                    false-value="maybe"
+                    indeterminate-value="false"
+                    toggle-order="tf"
+                    color="primary"
+                    label="On project"
+                    right-label
+                    @click="async () => await loadStudents($refs.infiniteScroll)"
                   />
                 </div>
-              </template>
-            </q-select>
-
-            <q-select
-              v-model="status"
-              rounded
-              outlined
-              dense
-              clearable
-              color="primary"
-              bg-color="white"
-              :options="stati"
-              :option-label="opt => opt.label"
-              :option-value="opt => opt.value"
-              label="Status"
-              emit-value
-              map-options
-              @update:model-value="async () => await loadStudents($refs.infiniteScroll)"
-            />
-
-            <div class="row q-gutter-x-md">
-              <q-checkbox
-                v-model="byMe"
-                toggle-indeterminate
-                false-value="maybe"
-                indeterminate-value="false"
-                toggle-order="tf"
-                color="primary"
-                label="Suggested by you"
-                right-label
-                @click="async () => await loadStudents($refs.infiniteScroll)"
-              />
-              <q-checkbox
-                v-model="onProject"
-                toggle-indeterminate
-                true-value="true"
-                false-value="maybe"
-                indeterminate-value="false"
-                toggle-order="tf"
-                color="primary"
-                label="On project"
-                right-label
-                @click="async () => await loadStudents($refs.infiniteScroll)"
-              />
-            </div>
-          </div>
               </div>
-            </q-slide-transition>
-          </div>
+            </div>
+          </q-slide-transition>
+        </div>
 
-            <q-scroll-area
-              class="fadeOut q-px-sm"
-              :thumb-style="thumbStyle"
-              style="flex: 1; overflow: auto;"
-              @scroll="onScroll"
-              
-            >
-              <q-infinite-scroll
-                ref="infiniteScroll"
-                class="q-pa-sm"
-                
-                :offset="250"
-                @load="async (index, done) => await studentStore.loadNext(index, done, filters)"
-              >
-                <StudentCard
-                  v-for="student in studentStore.students"
-                  :id="student.email"
-                  :key="student.email"
-                  
-                  class="q-ma-sm"
-                  :draggable="draggable"
-                  :must-hover="mustHover"
-                  :student="student"
-                  :active="studentStore.currentStudent ? student.email === studentStore.currentStudent.email : false"
-                  @click="clickStudent(student)"
-                  @dragstart="onDragStart($event, student)"
+        <q-scroll-area
+          class="fadeOut q-px-sm"
+          :thumb-style="thumbStyle"
+          style="flex: 1; overflow: auto;"
+          @scroll="onScroll"
+
+        >
+          <q-infinite-scroll
+            ref="infiniteScroll"
+            class="q-pa-sm"
+
+            :offset="250"
+            @load="async (index, done) => await studentStore.loadNext(index, done, filters)"
+          >
+            <StudentCard
+              v-for="student in studentStore.students"
+              :id="student.email"
+              :key="student.email"
+
+              class="q-ma-sm"
+              :draggable="draggable"
+              :must-hover="mustHover"
+              :student="student"
+              :active="studentStore.currentStudent ? student.email === studentStore.currentStudent.email : false"
+              @click="clickStudent(student)"
+              @dragstart="onDragStart($event, student)"
+            />
+            <template #loading>
+              <div class="row justify-center q-my-md">
+                <q-spinner-dots
+                  color="primary"
+                  size="40px"
                 />
-                <template #loading>
-                  <div class="row justify-center q-my-md">
-                    <q-spinner-dots
-                      color="primary"
-                      size="40px"
-                    />
-                  </div>
-                </template>
-              </q-infinite-scroll>
-            </q-scroll-area>
-          </div>
-        
-      
+              </div>
+            </template>
+          </q-infinite-scroll>
+        </q-scroll-area>
+      </div>
 
-        <q-inner-loading
-          :showing="studentStore.isLoading"
-          label="Please wait..."
-          label-class="text-teal"
-          label-style="font-size: 1.1em"
-        />
-      
+<!--      <q-inner-loading-->
+<!--        :showing="studentStore.isLoading"-->
+<!--        label="Please wait..."-->
+<!--        label-class="text-teal"-->
+<!--        label-style="font-size: 1.1em"-->
+<!--      />-->
 
       <div
         class="absolute"
@@ -211,7 +209,7 @@
         <btn
           dense
           shadow-color="yellow"
-          shadow-strength = "1"
+          shadow-strength="1"
           color="yellow"
           :icon="!miniState? 'chevron_left' : 'chevron_right'"
           @click="miniState = !miniState"
@@ -228,7 +226,7 @@ import StudentCard from "./StudentCard.vue";
 import {useStudentStore} from "../stores/useStudentStore";
 import stati from "../features/mails/Status";
 import {useQuasar} from "quasar";
-import { Student } from '../models/Student';
+import {Student} from '../models/Student';
 import {useSkillStore} from "../stores/useSkillStore";
 import StudentSkillChip from "../features/students/components/StudentSkillChip.vue";
 
@@ -310,13 +308,13 @@ export default defineComponent({
       if (this.byMe !== 'maybe') filter.suggested_by_user = this.byMe
       if (this.onProject !== 'maybe') filter.on_project = this.onProject
       if (this.status) filter.status = this.status
-      if (this.skills.length > 0) filter.skills = this.skills.map((skill: {id: number}) => skill.id)
+      if (this.skills.length > 0) filter.skills = this.skills.map((skill: { id: number }) => skill.id)
 
       return filter
     },
     showDrawer() {
       console.log(this.$route.name)
-        return this.$route.name === "Students" || this.$route.name === "Projects" || this.$route.name === "Student Page";
+      return this.$route.name === "Students" || this.$route.name === "Projects" || this.$route.name === "Student Page";
     }
   },
   methods: {
@@ -344,8 +342,6 @@ export default defineComponent({
       scroll.reset()
       scroll.resume()
       scroll.trigger()
-      // this.studentStore.students = []
-      // await this.studentStore.loadStudents(this.filters)
     },
   }
 })
@@ -353,9 +349,9 @@ export default defineComponent({
 
 <style scoped>
 :deep(.q-card) {
-    border-radius: 10px !important;
-  }
-  
+  border-radius: 10px !important;
+}
+
 :deep(.q-item) {
   padding: 8px 8px !important;
 }
