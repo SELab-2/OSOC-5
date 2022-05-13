99554bae33af91ef1666c2c9d3457e039a897167
Wouter01 <wouterhennen@gmail.com>
2022-05-13 22:42:09 +0200

    removed random function

--- fe/src/features/students/SelectStudentsPage.vue
+++ fe/src/features/students/SelectStudentsPage.vue
@@ -1,33 +1,12 @@
 <template>
   <div class="placeholder">
     <div class="q-mb-xl">
-        <Vue3Lottie :animationLink="options[Math.floor(Math.random() * options.length)]" :height="200" :width="200" />
+        <Vue3Lottie animationLink="https://assets8.lottiefiles.com/packages/lf20_av8ts5jt.json" :height="200" :width="200" />
         <h6 class="text-bold q-mt-none q-mb-xl">Select a student from the sidebar to get started</h6>
     </div>
   </div>
 </template>
 
-<script>
-
-const options = [
-  "https://assets8.lottiefiles.com/packages/lf20_av8ts5jt.json", // Pijl
-  // "https://assets7.lottiefiles.com/packages/lf20_lodmufpp.json", // Regenboog
-  // "https://assets4.lottiefiles.com/private_files/lf30_atq8bxjz.json", // Ogen
-  // "https://assets7.lottiefiles.com/private_files/lf30_w0rzeebl.json", // Confetti
-  // "https://assets5.lottiefiles.com/packages/lf20_kyusfkfp.json", // Banaan
-  // "https://assets5.lottiefiles.com/packages/lf20_zkhpkpyc.json", // Tomaat
-  // "https://assets7.lottiefiles.com/private_files/lf30_cdui7M.json" // Erwten
-]
-
-export default {
-  data() {
-    return {
-      options
-    }
-  },
-}
-
-</script>
 
 <style scoped>
 
