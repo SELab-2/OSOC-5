bc3b9d997810f3c485927c3453e0596853695330
Wouter01 <wouterhennen@gmail.com>
2022-05-13 22:09:47 +0200

    added lottie animations

--- fe/src/features/students/SelectStudentsPage.vue
+++ fe/src/features/students/SelectStudentsPage.vue
@@ -1,43 +1,42 @@
 <template>
-  <!-- <q-page-container style="height: 100%"> -->
-
-  <div>
-    <div class="q-pa-lg full-height flex-center">
-      <label>Select a student from the sidebar to get started</label>
+  <div class="placeholder">
+    <div class="q-mb-xl">
+        <Vue3Lottie :animationLink="options[Math.floor(Math.random() * options.length)]" :height="200" :width="200" />
+        <h6 class="text-bold q-mt-none q-mb-xl">Select a student from the sidebar to get started</h6>
     </div>
   </div>
-  <!-- </q-page-container> -->
 </template>
 
-<script lang="ts">
-import {ref} from "vue"
-import SideBar from "../../components/SideBar.vue"
-import {defineComponent} from "@vue/runtime-core";
+<script>
 
-export default defineComponent({
-  components: {
-    SideBar,
-  },
-  setup() {
-    const student = ref(null)
+const options = [
+  "https://assets8.lottiefiles.com/packages/lf20_av8ts5jt.json", // Pijl
+  "https://assets7.lottiefiles.com/packages/lf20_lodmufpp.json", // Regenboog
+  "https://assets4.lottiefiles.com/private_files/lf30_atq8bxjz.json", // Ogen
+  "https://assets7.lottiefiles.com/private_files/lf30_w0rzeebl.json", // Confetti
+  "https://assets5.lottiefiles.com/packages/lf20_kyusfkfp.json", // Banaan
+  "https://assets5.lottiefiles.com/packages/lf20_zkhpkpyc.json", // Tomaat
+  "https://assets7.lottiefiles.com/private_files/lf30_cdui7M.json" // Erwten
+  
+  
+]
 
+export default {
+  data() {
     return {
-      student,
+      options
     }
-  }
-})
+  },
+}
+
 </script>
 
-<style>
-.minimum_width {
-  min-width: 300px !important;
-}
+<style scoped>
 
-.studentcol {
-  padding: 5px;
-}
-
-.cornered {
-  border-radius: 10px !important
+.placeholder {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center 
 }
 </style>
