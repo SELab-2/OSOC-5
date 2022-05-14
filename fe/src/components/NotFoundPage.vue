b94ba2362e59b03e1669f30789fd3ceb6f9146ef
Wouter Hennen <62355975+Wouter01@users.noreply.github.com>
2022-05-14 14:56:29 +0200

    Improved select student placeholder + 404 page (#409)
    * added lottie animations
    
    * Removed most from selectstudent
    
    * removed random function

--- fe/src/components/NotFoundPage.vue
+++ fe/src/components/NotFoundPage.vue
@@ -1,13 +1,23 @@
 <template>
-  <h1 >404: Page not found.</h1>
+  <div class="fixed-center">
+        <Vue3Lottie :animationLink="options[Math.floor(Math.random() * options.length)]" :height="400" :width="400" />
+  </div>
 </template>
 
 <script lang="ts">
+
+const options = [
+  "https://assets8.lottiefiles.com/packages/lf20_GIyuXJ.json",
+  "https://assets1.lottiefiles.com/packages/lf20_zyu0ctqb.json",
+  "https://assets1.lottiefiles.com/packages/lf20_a3kesdek.json"
+]
+
 export default {
-  name: "NotFoundPage"
+  data() {
+    return {
+      options
+    }
+  },
 }
+
 </script>
-
-<style>
-
-</style>
