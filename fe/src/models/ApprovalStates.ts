864a713b39b6e36b421528878cf99b0dbc937f6a
Lisa De Jonghe <lisa.dejonghe@ugent.be>
2022-05-15 12:29:21 +0200

    Fe/mails changes (#415)
    * change mails, add list
    
    * improvements mails tab
    
    * try making qOptionGroup work
    
    * make radio buttons work
    
    * fix bug for applied
    
    * fix bug confirmed emails
    
    * fix infinite loading
    
    * fix computed property error
    
    * fix vue build errors
    
    * fix error
    
    * fix bug and adapt when state is passed
    
    * change red and !, fix little bug

---
+++ fe/src/models/ApprovalStates.ts
@@ -0,0 +1,34 @@
+const approvalStates = [
+    {
+        label: 'Applied',
+        value: 0,
+        icon: 'grade',
+    },
+    {
+        label: 'Awaiting',
+        value: 1,
+        icon: 'schedule',
+    },
+    {
+        label: 'Approved',
+        value: 2,
+        icon: 'check',
+    },
+    {
+        label: 'Contract Confirmed',
+        value: 3,
+        icon: 'playlist_add_check',
+    },
+    {
+        label: 'Contract Declined',
+        value: 4,
+        icon: 'playlist_add_check',
+    },
+    {
+        label: 'Rejected',
+        value: 5,
+        icon: 'check',
+    }
+]
+
+export default approvalStates
