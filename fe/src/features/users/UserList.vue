99c54ab3656b6e138f127c5146b5d85be56dcb7b
Wouter01 <wouterhennen@gmail.com>
2022-05-20 12:09:16 +0200

    TS fix

--- fe/src/features/users/UserList.vue
+++ fe/src/features/users/UserList.vue
@@ -127,7 +127,7 @@
                   </q-item>
                 </template>
               </q-select>
-              <div v-else>{{ roles.find(r => r.value === props.row.role).label }}</div>
+              <div v-else>{{ roles.find(r => r.value === props.row.role)!.label }}</div>
             </q-td>
             <q-td
               key="assignedto"
